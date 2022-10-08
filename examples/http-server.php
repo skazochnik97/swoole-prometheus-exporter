78a506d13abbdbba30c16d0aec48d54813cc8a82
Alexander Shelyakin <ashelyakin@sportmaster.ru>
2022-10-08 11:36:09 +0300

    initial commit

---
+++ examples/http-server.php
@@ -0,0 +1,21 @@
+<?php
+include '../vendor/autoload.php';
+
+use Swoole\Http\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+$server = new Swoole\HTTP\Server("127.0.0.1", 9501);
+
+$server->on("Start", function(Server $server)
+{
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
+
+$server->on("Request", function(Request $request, Response $response)
+{
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n");
+});
+
+$server->start();
---
+++ src/PrometheusExporter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Skazochnik97\SwoolePrometheusExporter;
+
+use Swoole\Http\Server;
+
+class PrometheusExporter
+{
+    const MIME_TYPE = 'text/plain; version=0.0.4';
+
+    const METADATA = <<<EOF
+# HELP %s %s
+# TYPE %1\$s %s
+EOF;
+
+    /**
+     * @var Swoole\HTTP\Server
+     */
+    private $server;
+
+    /**
+     * Constructor.
+     *
+     * @param Swoole\HTTP\Server    $server
+     */
+    private function __construct($server)
+    {
+        $this->server = $server;
+    }
+
+    /**
+     * Renders the samples in the Prometheus text format.
+     *
+     * @see https://prometheus.io/docs/instrumenting/exposition_formats/
+     * 
+     * @return string
+     */
+    public function render()
+    {
+        return "";
+    }
+}
